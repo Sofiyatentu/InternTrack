4020b6902c442bb35d2179e2133104cd2fc5320a
Sofiyatentu <sofiyatentu@gmail.com>
2026-03-05 19:59:00 +0530

    added authRoutes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,40 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, age, password } = req.body;
+    if (!name || !email || !age || !password)
+      return res.status(400).json({ message: "Please fill all the details." });
+    const existedEmail = await User.findOne({ email });
+    if (existedEmail)
+      return res.status(400).json({ message: "Email already exists." });
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ name, email, age, password: hashedPassword });
+    await newUser.save();
+    return res.status(201).json({
+      name: newUser.name,
+      age: newUser.age,
+      email: newUser.email,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error occurred." });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password)
+    return res.status(400).json({ message: "Please fill all the details." });
+  const existingUser = await User.findOne({ email });
+  if (!existingUser)
+    return res.status(404).json({ message: "Email doesn't exist." });
+  console.log(existingUser);
+  const isMatch = await bcrypt.compare(password, existingUser.password);
+  if (!isMatch)
+    return res.status(403).json({ message: "Invalid credentials." });
+  return res.status(200).json({ message: "Logged in successfully." });
+};
+
+module.exports = { registerUser, loginUser };
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+
 const User = require("./models/User");
+
+const authRouter = require("./routes/authRoutes");
+
 const app = express();
 const port = 5000;
 
@@ -13,30 +17,7 @@ mongoose
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hii future Intern...");
-});
-
-app.post("/register", async (req, res) => {
-  try {
-    const { name, age, email, password } = req.body;
-    if (!name || !age || !email || !password)
-      return res.status(400).json({ message: "Please fill all the details." });
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const newUser = new User({ name, age, email, password: hashedPassword });
-    await newUser.save();
-    console.log(newUser);
-    return res.status(201).json({
-      id: newUser._id,
-      name: newUser.name,
-      age: newUser.age,
-      email: newUser.email,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Internal server error." });
-  }
-});
+app.use("/auth", authRouter);
 
 app.listen(port, () => {
   console.log(`server running on http://localhost:${port}`);
