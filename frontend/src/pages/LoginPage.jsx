4930463f71054e89d1131c2817fe1ee030b86499
Sofiyatentu <sofiyatentu@gmail.com>
2026-03-18 23:04:34 +0530

    Made Login form

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
+
 function LoginPage() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleOnChange(e) {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
     <div>
-      <h1>LoginPage</h1>
+      <h1>LoginForm</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          placeholder="Enter your email"
+          onChange={handleOnChange}
+        />
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          placeholder="Enter password"
+          onChange={handleOnChange}
+        />
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
